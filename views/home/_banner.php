f8e482eb8dc3007bf52adcccbd3e344f9235c207
K <K>
2021-02-02 14:27:45 +0700

    Fix : Banner mobile and desktop

--- views/home/_banner.php
+++ views/home/_banner.php
@@ -2,5 +2,6 @@
 use yii\helpers\Url;
 ?>
 <div class='banner' style="cursor: pointer;">
-  	<img src="<?php echo Url::base(true); ?>/img/banner.jpg" width="100%">
+  	<img src="<?php echo Url::base(true); ?>/img/banner.jpg" width="100%" class="desktop">
+  	<img src="<?php echo Url::base(true); ?>/img/banner_mobile.jpg" width="100%" class="mobile">
 </div>
